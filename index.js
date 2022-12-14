2f92361ef514bcbc38d51a8cde132798a57bafeb
+ <+>
2022-12-14 14:53:43 +0300

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const popular = document.querySelector('#popular .disp')
 const neww = document.querySelector('#new .disp')
 const local = document.querySelector('#local .disp')
+function fetchData(){
+    return fetch('https://my-json-server.typicode.com/alishahmohamed/coctail-lovers/cocktails')
+    .then(response => response.json())
+    .then(json => json)
+}
